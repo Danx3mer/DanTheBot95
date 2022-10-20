0dd7946c9356812449671b16f81d6d171d13aec1
~Ð∆Ñ}{3₥€®~ <81121522+Danx3mer@users.noreply.github.com>
2022-10-20 01:48:43 +0000

    Lock and Unlock commands fully functional!

---
+++ Commands/Public/lock.js
@@ -0,0 +1,43 @@
+const {
+	SlashCommandBuilder,
+	CommandInteraction,
+	Colors,
+	PermissionFlagsBits
+} = require("discord.js");
+
+const createEmbed = require("../../Tools/Embed.js")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("lock")
+		.setDescription("Locks the specified channel so that only moderators can type.")
+		.addChannelOption(option =>
+			option.setName('channel')
+				.setDescription('The channel that you want to lock down.'))
+		.addStringOption(option =>
+			option.setName('reason')
+				.setDescription('The reason that you want to lock the channel down.')),
+	/**
+   *
+   * @param {CommandInteraction} interaction
+   */
+	execute(interaction) {
+		const channel = interaction.options.getChannel('channel') || interaction.channel;
+		const reason = interaction.options.getString('reason') || "No reason";
+
+		if (!channel.permissionsFor(interaction.guild.id).has(PermissionFlagsBits.SendMessages))
+			return interaction.reply({
+				embeds:
+					[createEmbed("The channel is already locked!", "Already Locked!", "", "", Colors.Yellow)]
+				, ephemeral: true
+			});
+
+		channel.permissionOverwrites.edit(interaction.guild.id, {
+			SendMessages: false,
+		});
+		return interaction.reply({
+			embeds:
+				[createEmbed(`${channel} has been locked! Reason: ${reason}`, "Locked!", "", "", Colors.Red)]
+		})
+	},
+};
---
+++ Commands/Public/unlock.js
@@ -0,0 +1,39 @@
+const {
+	SlashCommandBuilder,
+	CommandInteraction,
+	Colors,
+	PermissionFlagsBits
+} = require("discord.js");
+
+const createEmbed = require("../../Tools/Embed.js")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("unlock")
+		.setDescription("Unlocks the specified channel so that everyone can type.")
+		.addChannelOption(option =>
+			option.setName('channel')
+				.setDescription('The channel that you want to unlock.')),
+	/**
+   *
+   * @param {CommandInteraction} interaction
+   */
+	execute(interaction) {
+		const channel = interaction.options.getChannel('channel') || interaction.channel;
+
+		if (channel.permissionsFor(interaction.guild.id).has(PermissionFlagsBits.SendMessages))
+			return interaction.reply({
+				embeds:
+					[createEmbed("The channel isn\'t locked!", "Not Locked!", "", "", Colors.Yellow)]
+				, ephemeral: true
+			});
+
+		channel.permissionOverwrites.edit(interaction.guild.id, {
+			SendMessages: true,
+		});
+		return interaction.reply({
+			embeds:
+				[createEmbed(`${channel} has been unlocked!`, "Unlocked!", "", "", Colors.Green)]
+		})
+	},
+};
