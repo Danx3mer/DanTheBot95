fdb1ffd6d2d87a3fde8abe4e43d2064bb642d088
DanTheMan95 <12266294-Danx3mer@users.noreply.replit.com>
2024-05-14 22:02:41 +0000

    Restart of bot due to discord account being banned and updates of a lot of packages

--- Events/interactions/interactionCreate.js
+++ Events/interactions/interactionCreate.js
@@ -1,39 +1,39 @@
 const { CommandInteraction } = require("discord.js");
 
 module.exports = {
-  name: "interactionCreate",
-  /**
-   *
-   * @param {CommandInteraction} interaction
-   */
-  execute(interaction, client) {
-    if(interaction.isChatInputCommand()) {
+	name: "interactionCreate",
+	/**
+	 *
+	 * @param {CommandInteraction} interaction
+	 */
+	execute(interaction, client) {
+		if (interaction.isChatInputCommand()) {
 
-    const command = client.commands.get(interaction.commandName);
+			const command = client.commands.get(interaction.commandName);
 
-    if (!command)
-      return interaction.reply({
-        content: "This command is outdated/ no longer supported.",
-      });
+			if (!command)
+				return interaction.reply({
+					content: "This command is outdated/ no longer supported.",
+				});
 
-    command.execute(interaction, client);
-    } 
-    else if(interaction.isButton()) {
-      const { buttons } = client
-      const { customId } = interaction
+			command.execute(interaction, client);
+		}
+		else if (interaction.isButton()) {
+			const { buttons } = client
+			const { customId } = interaction
 
 			if (!customId.endsWith(interaction.user.id)) {
-  return interaction.reply({
-    content: "This button is not for you",
-    ephemeral: true
-  })
-}
-			let newCustomId=customId.toString().replace(interaction.user.id.toString(), '');
-      const button = buttons.get(newCustomId)
-      if(!button) return console.log("ERROR: THERE IS NO CODE FOR THIS BUTTON")
-      try {
-        button.execute(interaction, client)
-      } catch(e) { console.error(e) }
-    }
-  },
+				return interaction.reply({
+					content: "This button is not for you",
+					ephemeral: true
+				})
+			}
+			let newCustomId = customId.toString().replace(interaction.user.id.toString(), '');
+			const button = buttons.get(newCustomId)
+			if (!button) return console.log("ERROR: THERE IS NO CODE FOR THIS BUTTON")
+			try {
+				button.execute(interaction, client)
+			} catch (e) { console.error(e) }
+		}
+	},
 };
